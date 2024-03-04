7dfb74e5d1b86dd996ac624ad03977f702970156
Diogo Jonsson <diogo.jonsson@hotmail.com>
2024-03-04 19:55:22 -0300

    change symbols

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,7 +6,7 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activeSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -14,9 +14,10 @@ export default function GameBoard() {
       const updatedGameBoard = [
         ...prevGameBoard.map((innerArray) => [...innerArray]),
       ];
-      updatedGameBoard[rowIndex][colIndex] = "X";
+      updatedGameBoard[rowIndex][colIndex] = activeSymbol;
       return updatedGameBoard;
     });
+    onSelectSquare();
   }
 
   return (
